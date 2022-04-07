8fbd3bd8be59fc65ccda18dba5dc4c2022f22ba8
akcaau <gllazrv@gmail.com>
2022-04-07 23:50:19 +0300

    Добавлена связь шаблонов базы

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
     template = 'posts/index.html'
     title = 'Главная'
-    header = 'Все записи'
+    header = 'Лента новостей'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context ={
         'title': title,
         'header': header,
+        'posts': posts,
     }
     return render(request, template, context)
 
@@ -14,9 +17,11 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     title = 'группа'
     header = 'Записи группы'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context ={
         'title': title,
         'header': header,
+        'posts': posts,
     }
     return render(request, template, context)
     
