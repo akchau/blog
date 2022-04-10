baa5c7ee1652d9e27c505986438716d5cac877f0
akcaau <gllazrv@gmail.com>
2022-04-10 16:16:00 +0300

    Добавлены страницы групп и настроен переход по ссылке

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
     template = 'posts/index.html'
@@ -14,10 +14,11 @@ def index(request):
     return render(request, template, context)
 
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
     title = 'группа'
-    header = 'Записи группы'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    header = f'Записи группы {group.title}'
     context ={
         'title': title,
         'header': header,
