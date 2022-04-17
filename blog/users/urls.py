5150d528760d0ae56218fe166945422cbc9d8dbe
akcaau <gllazrv@gmail.com>
2022-04-17 18:10:23 +0300

    Добавлен личный кабинет, добавлена навигация

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
 
     # Сообщение об успешном восстановлении пароля
     path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
+    path('cabinet/<slug:username>/', views.cabinet, name='cabinet')
 ] 
--- blog/users/views.py
+++ blog/users/views.py
@@ -1,8 +1,22 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CreationForm
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
 
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
+
+def cabinet(request, username):
+    User = get_user_model()
+    author = get_object_or_404(User, username=username)
+    title = 'Личный кабинет'
+    header = 'Личный кабинет'
+    template = 'users/cabinet.html'
+    context ={
+        'title': title,
+        'header': header,
+    }
+    return render(request, template, context)
